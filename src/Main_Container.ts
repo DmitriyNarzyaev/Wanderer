eab01d1053a9e16acdb1d88d8fb165d66c517056
Dmitriy Narzyaev <nozaki@yandex.ru>
2025-07-02 00:26:09 +0300

    added player moving

--- src/Main_Container.ts
+++ src/Main_Container.ts
@@ -1,10 +1,15 @@
 import { Graphics } from "pixi.js";
 import Container = PIXI.Container;
 import Player from "./Player";
+import Global from "./Global";
 
 export default class Main_Container extends Container {
 	public static readonly WIDTH:number = 1920;
 	public static readonly HEIGHT:number = 1080;
+	private BUTTON_LEFT:boolean = false;
+	private BUTTON_RIGHT:boolean = false;
+	private BUTTON_UP:boolean = false;
+	private BUTTON_DOWN:boolean = false;
 	private _background: Graphics;
 	private _player:Player;
 
@@ -12,6 +17,16 @@ export default class Main_Container extends Container {
 		super();
 		this.initialBackground();
 		this.initialPlayer();
+
+		window.addEventListener("keydown",
+			(e:KeyboardEvent) => {this._player
+			this.keyDownHandler(e);
+		},);
+		window.addEventListener("keyup",
+			(e:KeyboardEvent) => {
+			this.keyUpHandler(e);
+		},);
+		Global.PIXI_APP.ticker.add(this.ticker, this);
 	}
 
 	private initialBackground():void {
@@ -25,6 +40,52 @@ export default class Main_Container extends Container {
 		this._player = new Player;
 		this._player.x = Main_Container.WIDTH/2 - this._player.width/2;
 		this._player.y = Main_Container.HEIGHT/2 - this._player.height/2;
+		this._player.interactive = true;
 		this.addChild(this._player);
+	}
+
+	private keyDownHandler(e:KeyboardEvent):void {
+		if (e.code == "ArrowLeft") {
+			this.BUTTON_LEFT = true;
+		}
+		if (e.code == "ArrowRight") {
+			this.BUTTON_RIGHT = true;
+		}
+		if (e.code == "ArrowUp") {
+			this.BUTTON_UP = true;
+		}
+		if (e.code == "ArrowDown") {
+			this.BUTTON_DOWN = true;
+		}
+	}
+
+	private keyUpHandler(e:KeyboardEvent):void {
+		if (e.code == "ArrowLeft") {
+			this.BUTTON_LEFT = false;
+		}
+		if (e.code == "ArrowRight") {
+			this.BUTTON_RIGHT = false;
+		}
+		if (e.code == "ArrowUp") {
+			this.BUTTON_UP = false;
+		}
+		if (e.code == "ArrowDown") {
+			this.BUTTON_DOWN = false;
+		}
+    }
+
+	private ticker():void {
+		if (this.BUTTON_LEFT == true) {
+			this._player.x -= this._player.playerSpeed;
+		}
+		if (this.BUTTON_RIGHT == true) {
+			this._player.x += this._player.playerSpeed;
+		}
+		if (this.BUTTON_UP == true) {
+			this._player.y -= this._player.playerSpeed;
+		}
+		if (this.BUTTON_DOWN == true) {
+			this._player.y += this._player.playerSpeed;
+		}
 	}
 }
--- src/Player.ts
+++ src/Player.ts
@@ -2,6 +2,7 @@ import { Graphics } from "pixi.js";
 import Container = PIXI.Container;
 
 export default class Player extends Container {
+    public playerSpeed:number = 2.5;
     private _playerSize:number = 20;
     private _eyeSize:number = 4;
 
@@ -11,17 +12,17 @@ export default class Player extends Container {
     }
 
     private initialPlayer():void {
-        let player: PIXI.Graphics = new PIXI.Graphics;
+        let player: Graphics = new Graphics;
         player.beginFill(0x999999);
         player.drawCircle(0, 0, this._playerSize);
         this.addChild(player);
 
-        let eyeL: PIXI.Graphics = new PIXI.Graphics;
+        let eyeL: Graphics = new Graphics;
         player.beginFill(0x992222);
         player.drawCircle(-8, -5, this._eyeSize);
         this.addChild(eyeL);
 
-        let eyeR: PIXI.Graphics = new PIXI.Graphics;
+        let eyeR: Graphics = new Graphics;
         player.beginFill(0x992222);
         player.drawCircle(8, -5, this._eyeSize);
         this.addChild(eyeR);
